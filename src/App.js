15d55cb5a3a90837e537c13444b53f8ad2fd02e5
unknown <k.jones45@yahoo.com>
2023-04-21 00:48:25 -0400

    Added code to MudicTable file to display songs on browser. + Created a addASong function and a button.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MusicTable from './Components/MusicTable/MusicTable';
 
 
 function App() { 
-  const [songs, setSongs] = useState([]);
+  const [songs, setSongs] = useState([{title: "Just The Way You Are", album: "DooWaps and Hooligans", artist: "Bruno Mars", genre: "Pop", release_date: "12-04-2010"}])
+
   
   useEffect(() => {
     getAllSongs();
-    console.log('Hello World!');
+    addAsong();
+    {/*deleted console.log("Hello World!") */}
+    
    
 
   },[]);
+
   
   async function getAllSongs(){
     const response = await axios.get('http://127.0.0.1:8000/api/music/');
     console.log (response.data);
     setSongs(response.data)
-
   }
+
+    async function addAsong(){
+      const response = await axios.post('http://127.0.0.1:8000/api/music/');
+      console.log (response.data);
+      setSongs(response.data)
+
+    }
+  
+
 
   return (
     <div>
       <button onClick={() => getAllSongs()}>Get All Songs</button>
+      <MusicTable parentEntries= {songs}/>
+      <button onClick={() => addAsong()}>Add A Song</button>
       
     
     </div>
@@ -29,4 +44,5 @@ function App() {
 }
 
 export default App;
-{/*can use the state variable setSongs to help map through and display each song in a table  */}
+
+
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,39 @@
+
+const MusicTable = (props) => {
+    return ( 
+        <table className="table">
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>album</th>
+            <th>artist</th>
+            <th>genre</th>
+            <th>release_date</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry, index)=> {
+            return (
+              <tr key={index}> {/* if you swap this out for date that will give us the date = bonus question */}
+                <td>{index + 1}</td>
+                <td>{entry.title}</td>
+                <td>{entry.album}</td>
+                <td>{entry.artist}</td>
+                <td>{entry.genre}</td>
+                <td>{entry.release_date}</td>
+              </tr>
+            )
+          })}
+    
+        </tbody>
+      </table>
+     );
+}
+ 
+export default MusicTable;
+
+
+
+{/*Copied DisplayedEntries component into MusicTable component to see if this 
+will generate the table  */}
